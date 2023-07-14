a1e245c5363970316eca3e0845ee50de4781098b
Tristan <tristan.lieberherr@heig-vd.ch>
2023-07-14 08:20:23 +0200

    added function codes in switch

--- src/node.cpp
+++ src/node.cpp
@@ -1,6 +1,6 @@
 #include "node.hpp"
 
-CANopen_Node::CANopen_Node(int id) : nodeId(id), nmt(*this), hb(*this)
+CANopen_Node::CANopen_Node(int id) : nodeId(id), nmt(*this), hb(*this), sdo(*this)
 {
 }
 
@@ -10,6 +10,26 @@ void CANopen_Node::receiveFrame(CANopen_Frame frame)
     {
     case FunctionCode_NMT:
         nmt.receiveFrame(frame);
+        break;
+    case FunctionCode_SYNC: // Also FunctionCode_EMCY
+        break;
+    case FunctionCode_TIME:
+        break;
+    case FunctionCode_TPDO1:
+    case FunctionCode_TPDO2:
+    case FunctionCode_TPDO3:
+    case FunctionCode_TPDO4:
+        break;
+    case FunctionCode_RPDO1:
+    case FunctionCode_RPDO2:
+    case FunctionCode_RPDO3:
+    case FunctionCode_RPDO4:
+        break;
+    case FunctionCode_TSDO:
+    case FunctionCode_RSDO:
+        sdo.receiveFrame(frame);
+        break;
+    case FunctionCode_HEARTBEAT:
         break;
     }
 }
