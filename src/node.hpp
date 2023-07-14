fa00d9780ef7721c85722c89f68ef4a920e0f2a2
Tristan <tristan.lieberherr@heig-vd.ch>
2023-07-14 08:20:35 +0200

    added sdo object

--- src/node.hpp
+++ src/node.hpp
@@ -2,6 +2,7 @@
 #include "frame.hpp"
 #include "nmt.hpp"
 #include "heartbeat.hpp"
+#include "sdo.hpp"
 
 class CANopen_Node
 {
@@ -9,10 +10,12 @@ private:
     int nodeId;
     CANopen_NMT nmt;
     CANopen_HB hb;
+    CANopen_SDO sdo;
 
 public:
     friend CANopen_NMT;
     friend CANopen_HB;
+    friend CANopen_SDO;
     CANopen_Node(int id);
     void receiveFrame(CANopen_Frame frame);
     void sendFrame(CANopen_Frame frame);
