04036891636ac501cce941855effae69db4c9d7b
Tristan <tristan.lieberherr@heig-vd.ch>
2023-07-18 15:48:21 +0200

    WIP

--- src/pdo.cpp
+++ src/pdo.cpp
@@ -1,7 +1,31 @@
 #include "pdo.hpp"
 #include "node.hpp"
+#include <cstring>
+#include <cstdio>
 
 CANopen_PDO::CANopen_PDO(CANopen_Node &node) : node(node)
+{
+    for (unsigned i = 0; i < OD_TPDO_COUNT; i++)
+    {
+        // OD_ObjectEntry *communication = node.od.findEntry(OD_TPDO_COMMUNICATION_INDEX + i);
+        OD_ObjectEntry *mapping = node.od.findEntry(OD_TPDO_MAPPING_INDEX + i);
+
+        tpdos[i].count = *(uint8_t *)mapping->objects[0].valueSrc;
+        printf("[PDO] TPDO[%d]\n", i + 1);
+        for (unsigned j = 0; j < tpdos[i].count; j++)
+        {
+            uint32_t content = *(uint32_t *)mapping->objects[j + 1].valueSrc;
+            uint16_t index = content >> 16;
+            uint8_t subIndex = content >> 8;
+            uint8_t size = content;
+            tpdos[i].sources[j] = (OD_Object *)node.od.findEntry(index)->objects + subIndex;
+            printf("index: 0x%04X, sub-index: %d, size(bits): %d\n", index, subIndex, size);
+        }
+        puts("");
+    }
+}
+
+void CANopen_PDO::update(uint32_t timestamp_us)
 {
 }
 
@@ -9,6 +33,13 @@ void CANopen_PDO::receiveFrame(CANopen_Frame frame)
 {
 }
 
-void CANopen_PDO::update(uint32_t timestamp_us)
+void CANopen_PDO::build(PDOMap tpdo, uint8_t *buffer)
 {
+    unsigned bytesWritten = 0;
+    for (unsigned i = 0; i < tpdo.count; i++)
+    {
+        OD_Object *obj = tpdo.sources[i];
+        memcpy(buffer + bytesWritten, obj->valueSrc, obj->size);
+        bytesWritten += obj->size;
+    }
 }
--- src/pdo.hpp
+++ src/pdo.hpp
@@ -2,11 +2,22 @@
 #include <cstdint>
 #include "enums.hpp"
 #include "frame.hpp"
+#include "od.hpp"
+
+struct PDOMap
+{
+    uint8_t count;
+    OD_Object *sources[8];
+};
 
 class CANopen_PDO
 {
 private:
     class CANopen_Node &node;
+
+    PDOMap tpdos[OD_TPDO_COUNT];
+
+    void build(PDOMap tpdo, uint8_t *buffer);
 
 public:
     CANopen_PDO(class CANopen_Node &node);
