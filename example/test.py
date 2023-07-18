e3cc9f6a6a10697795bd76310c2f868fbd1b0364
Tristan <tristan.lieberherr@heig-vd.ch>
2023-07-18 15:46:41 +0200

    .

--- example/test.py
+++ example/test.py
@@ -4,46 +4,35 @@ import struct
 
 
 
-
-states = ['OPERATIONAL', 'STOPPED', 'INITIALISING']
-
-
 network = canopen.Network()
 node = canopen.Node(4, "../generator/example.eds")
+
+
+entries = [entry for entry in node.object_dictionary.items() if 0x1000 <= entry[0] <= 0x1FFF]
+
+
+for index, entry in entries:
+    print(f"[%X] {entry.name}" % index)
+
+
+exit()
+
+
+
+
 network.add_node(node)
 network.connect(channel='vcan0', bustype='socketcan')
-
-
-# This will attempt to read an SDO from nodes 1 - 127
-# network.scanner.search()
-# sleep(0.05)
-# for node_id in network.scanner.nodes: print("Found node %d!" % node_id)
-
-# for state in states:
-#     node.nmt.state = state
-#     sleep(3)
-
-index = 0x6048
-subIndex = 1
-value = 3.1415
-bytelen = 8
-
 try:
-    node.sdo[0x1017].phys = 1000
-except (canopen.SdoCommunicationError, canopen.SdoAbortedError)  as e: print(e)
+    pass
 
 
 
+    # network.scanner.search()
+    # sleep(0.05)
+    # for node_id in network.scanner.nodes: print("Found node %d!" % node_id)
+
+    # for state in ['OPERATIONAL', 'STOPPED', 'INITIALISING']:
+    #     node.nmt.state = state
+    #     sleep(3)
+except Exception as e: print(e)
 network.disconnect()
-
-
-
-# print(int.from_bytes(node.sdo.upload(index, subIndex), 'little'))
-# node.sdo.download(index, subIndex, value.to_bytes(bytelen, 'little'))
-# print(int.from_bytes(node.sdo.upload(index, subIndex), 'little'))
-
-# network.send_message(0x604, bytes([0x40, 0x4A, 0x60, 0x01, 0x00, 0x00, 0x00, 0x00]))
-
-# print(struct.unpack("<d", node.sdo.upload(index, subIndex))[0])
-# node.sdo.download(index, subIndex, struct.pack("<d", value))
-# print(struct.unpack("<d", node.sdo.upload(index, subIndex))[0])
