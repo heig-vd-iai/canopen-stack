5672b8fda19083370331376a8103f6ca6f0f6b45
Tristan <tristan.lieberherr@heig-vd.ch>
2023-07-20 08:40:43 +0200

    created test folder for quick testing

---
+++ test/od_classes.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <cstdint>
+
+struct ObjectEntry
+{
+    const void *dataSrc;
+    const uint8_t accessType;
+    const uint8_t dataType;
+    const uint8_t size;
+
+    ObjectEntry(void *dataSrc, uint8_t accessType, uint8_t dataType, uint8_t size) : dataSrc(dataSrc), accessType(accessType), dataType(dataType), size(size) {}
+};
+
+struct Object
+{
+    const uint16_t index;
+    const uint8_t subNumber;
+    const uint16_t objectType;
+    const ObjectEntry *entries;
+
+    Object(uint16_t index, uint8_t subNumber, uint16_t objectType, ObjectEntry entries[]) : index(index), subNumber(subNumber), objectType(objectType), entries(entries) {}
+    virtual bool writeBytes(uint8_t subindex, uint8_t bytes[], unsigned size);
+};
+
+struct TPDOCommunicationObject : public Object
+{
+};
+
+struct TPDOMappingObject : public Object
+{
+};
---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+#include <cstdint>
+#include <cstdio>
+#include <iostream>
+#include "od_classes.hpp"
+using namespace std;
+
+
+
+
+int main()
+{
+    return 0;
+}
