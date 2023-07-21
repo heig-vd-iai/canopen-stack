6bf3e379e38f64dcc1aed92d3bd530987a8da927
Tristan <tristan.lieberherr@heig-vd.ch>
2023-07-21 13:38:29 +0200

    copy paste typo

--- src/objects/object_1A00.hpp
+++ src/objects/object_1A00.hpp
@@ -1,12 +1,12 @@
 #pragma once
 #include "od_classes.hpp"
-#define X1800_INDEX_COUNT 0
+#define X1A00_INDEX_COUNT 0
 
 class TPDOMappingObject : public Object
 {
 public:
     TPDOMappingObject(uint16_t index, uint8_t subNumber, uint16_t objectType, ObjectEntry *entries) : Object(index, subNumber, objectType, entries) {}
 
-    uint8_t getCount() { return *(uint8_t *)entries[X1800_INDEX_COUNT].dataSrc; }
+    uint8_t getCount() { return *(uint8_t *)entries[X1A00_INDEX_COUNT].dataSrc; }
     uint32_t getMappedValue(uint8_t index) { return *(uint32_t *)entries[index + 1].dataSrc; }
 };
