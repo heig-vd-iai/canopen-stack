f13d5f1a40d9180b8ff2587144691eca8f59b880
Tristan <tristan.lieberherr@heig-vd.ch>
2023-07-21 13:37:50 +0200

    added methods

--- src/objects/object_1A00.hpp
+++ src/objects/object_1A00.hpp
@@ -1,7 +1,12 @@
 #pragma once
 #include "od_classes.hpp"
+#define X1800_INDEX_COUNT 0
 
-struct TPDOMappingObject : public Object
+class TPDOMappingObject : public Object
 {
+public:
     TPDOMappingObject(uint16_t index, uint8_t subNumber, uint16_t objectType, ObjectEntry *entries) : Object(index, subNumber, objectType, entries) {}
+
+    uint8_t getCount() { return *(uint8_t *)entries[X1800_INDEX_COUNT].dataSrc; }
+    uint32_t getMappedValue(uint8_t index) { return *(uint32_t *)entries[index + 1].dataSrc; }
 };
