ce861f4cd326cb8b716406add14a8584a17afa0a
Tristan <tristan.lieberherr@heig-vd.ch>
2023-07-21 09:18:37 +0200

    .

--- example/test.py
+++ example/test.py
@@ -4,8 +4,7 @@ import struct
 
 
 def callback(message):
-    for var in message:
-        print('%s = %d' % (var.name, var.raw))
+    for var in message: print('%s = %d' % (var.name, var.raw))
 
 
 
@@ -23,18 +22,28 @@ subindex = 5
 network.add_node(node)
 network.connect(channel='vcan0', bustype='socketcan')
 try:
-    val = int.from_bytes(node.sdo.upload(index, subindex), 'little')
-    print(val)
-    node.sdo.download(index, subindex, (val + 1).to_bytes(2, 'little'))
-    val = int.from_bytes(node.sdo.upload(index, subindex), 'little')
-    print(val)
+    # val = int.from_bytes(node.sdo.upload(index, subindex), 'little')
+    # print(val)
+    # node.sdo.download(index, subindex, (val + 1).to_bytes(2, 'little'))
+    # val = int.from_bytes(node.sdo.upload(index, subindex), 'little')
+    # print(val)
 
+    # node.sdo.download(0x1800, 2, (0xF1).to_bytes(1, 'little'))
+    # node.sdo.download(0x1800, 2, (0xFB).to_bytes(1, 'little'))
+    print("Before")
+    print("COB-ID: " + hex(int.from_bytes(node.sdo.upload(0x1800, 1), 'little')))
+    print("Inhibit time: " + str(int.from_bytes(node.sdo.upload(0x1800, 3), 'little')))
+    print("After")
+    node.tpdo.read()
+    node.tpdo[1].add_callback(callback)
+    node.tpdo[1].event_timer = 1000
+    node.tpdo[1].enabled = False
+    node.tpdo.save()
+    print("COB-ID: " + hex(int.from_bytes(node.sdo.upload(0x1800, 1), 'little')))
+    node.tpdo[1].inhibit_time = 200
+    node.tpdo.save()
+    print("Inhibit time: " + str(int.from_bytes(node.sdo.upload(0x1800, 3), 'little')))
 
-    # node.tpdo.read()
-    # # node.sdo.download(0x1800, 5, (0).to_bytes(2, 'little'))
-    # node.tpdo[2].add_callback(callback)
-    # node.tpdo[2].event_timer = 1000
-    # node.tpdo.save()
     # network.nmt.state = "OPERATIONAL"
     # sleep(3)
 
