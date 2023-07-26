d737f2e02d606d8183da6ae608a5fd61a44a8bfa
Tristan <tristan.lieberherr@heig-vd.ch>
2023-07-26 11:24:20 +0200

    sync  counter & pointer to obj1019 (unused now)

--- src/sync.cpp
+++ src/sync.cpp
@@ -1,14 +1,15 @@
 #include "sync.hpp"
 #include "node.hpp"
-#include <cstdio>
+#include <cstdlib>
 using namespace CANopen;
 
-SYNC::SYNC(Node &node) : node(node)
+SYNC::SYNC(Node &node) : node(node), syncCounterOverflow(NULL)
 {
+    syncCounterOverflow = node.findObject(SYNC_OBJECT_1019);
 }
 
 void SYNC::receiveFrame(Frame frame, uint32_t timestamp_us)
 {
-    uint8_t counter = frame.dlc > 0 ? frame.data[0] : 0;
-    node.pdo.onSync(counter, timestamp_us);
+    internalCounter = frame.dlc > 0 ? frame.data[0] : internalCounter % SYNC_MAX_COUNTER + 1;
+    node.pdo.onSync(internalCounter, timestamp_us);
 }
--- src/sync.hpp
+++ src/sync.hpp
@@ -2,6 +2,8 @@
 #include <cstdint>
 #include "enums.hpp"
 #include "frame.hpp"
+#define SYNC_OBJECT_1019 0x1019
+#define SYNC_MAX_COUNTER 240
 
 namespace CANopen
 {
@@ -9,6 +11,8 @@ namespace CANopen
     {
     private:
         class Node &node;
+        class Object *syncCounterOverflow;
+        uint8_t internalCounter = 1;
 
     public:
         SYNC(class Node &node);
