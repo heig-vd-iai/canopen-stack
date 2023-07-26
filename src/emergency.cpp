0f1abb0a00e36c73c4954e4777e879a26a170b2e
Tristan <tristan.lieberherr@heig-vd.ch>
2023-07-26 11:42:36 +0200

    added emergency.cpp

---
+++ src/emergency.cpp
@@ -0,0 +1,13 @@
+#include "emergency.hpp"
+#include "node.hpp"
+using namespace CANopen;
+
+EMCY::EMCY(Node &node) : node(node) {}
+
+void EMCY::receiveFrame(Frame frame)
+{
+}
+
+void EMCY::publishError(uint16_t errorCode, uint8_t errorRegister)
+{
+}
