5963ffb5c5f8b78f5e031eb7f3e6b97127fdb158
Tristan <tristan.lieberherr@heig-vd.ch>
2023-08-04 10:56:59 +0200

    added verification of access type

--- generator/objects/object_1003.py
+++ generator/objects/object_1003.py
@@ -5,4 +5,9 @@ class Object1003(ArrayObject):
         super().__init__(index, entries, "Object1003")
 
     def verify(self, objects: dict) -> bool:
-        return True
+        retval = True
+        for i, entry in enumerate(self.entries[2:]):
+            if entry.accessTypeStr not in ["ro", "const"]:
+                print(f"[Error] Entry {self.varName}: access type of sub {i + 2} is not 'ro' ({entry.accessTypeStr})")
+                retval = False
+        return retval
