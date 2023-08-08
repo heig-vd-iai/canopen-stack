c382ec436acf17f8e0bdd44a089dcec022c0ddd9
Tristan <tristan.lieberherr@heig-vd.ch>
2023-08-08 16:12:54 +0200

    removed writeErrorBit (different behaviours)

--- src/objects/object_1001.cpp
+++ src/objects/object_1001.cpp
@@ -1,36 +1,6 @@
 #include "object_1001.hpp"
 using namespace CANopen;
 
-void Object1001::writeErrorBit(unsigned bit, bool value)
-{
-    ErrorRegister reg = {getValue()};
-    switch (bit)
-    {
-    case ErrorRegisterBit_Generic:
-        reg.bits.genericError = value;
-        break;
-    case ErrorRegisterBit_Current:
-        reg.bits.current = value;
-        break;
-    case ErrorRegisterBit_Voltage:
-        reg.bits.voltage = value;
-        break;
-    case ErrorRegisterBit_Temperature:
-        reg.bits.temperature = value;
-        break;
-    case ErrorRegisterBit_Communication:
-        reg.bits.communicationError = value;
-        break;
-    case ErrorRegisterBit_DeviceProfile:
-        reg.bits.deviceProfileSpecific = value;
-        break;
-    case ErrorRegisterBit_Manufacturer:
-        reg.bits.manufacturerSpecific = value;
-        break;
-    }
-    setValue(0, reg.value);
-}
-
 uint8_t Object1001::getValue()
 {
     return *(uint8_t *)entries[0].dataSrc;
@@ -38,13 +8,62 @@ uint8_t Object1001::getValue()
 
 void Object1001::setErrorBit(unsigned bit)
 { // Generic error shall be signaled at any error situation
-    writeErrorBit(bit, true);
-    writeErrorBit(ErrorRegisterBit_Generic, true);
+    ErrorRegister reg = {getValue()};
+    reg.bits.genericError = 1;
+    switch (bit)
+    {
+    case ErrorRegisterBit_Current:
+        reg.bits.current = 1;
+        break;
+    case ErrorRegisterBit_Voltage:
+        reg.bits.voltage = 1;
+        break;
+    case ErrorRegisterBit_Temperature:
+        reg.bits.temperature = 1;
+        break;
+    case ErrorRegisterBit_Communication:
+        reg.bits.communicationError = 1;
+        break;
+    case ErrorRegisterBit_DeviceProfile:
+        reg.bits.deviceProfileSpecific = 1;
+        break;
+    case ErrorRegisterBit_Manufacturer:
+        reg.bits.manufacturerSpecific = 1;
+        break;
+    }
+    setValue(0, reg.value);
 }
 
 void Object1001::clearErrorBit(unsigned bit)
 {
-    writeErrorBit(bit, false);
+    ErrorRegister reg = {getValue()};
+    switch (bit)
+    {
+    case ErrorRegisterBit_Generic:
+        // Only clear generic error bit is all other bits are clear
+        if (reg.value == 0b00000001)
+            reg.bits.genericError = 0;
+        break;
+    case ErrorRegisterBit_Current:
+        reg.bits.current = 0;
+        break;
+    case ErrorRegisterBit_Voltage:
+        reg.bits.voltage = 0;
+        break;
+    case ErrorRegisterBit_Temperature:
+        reg.bits.temperature = 0;
+        break;
+    case ErrorRegisterBit_Communication:
+        reg.bits.communicationError = 0;
+        break;
+    case ErrorRegisterBit_DeviceProfile:
+        reg.bits.deviceProfileSpecific = 0;
+        break;
+    case ErrorRegisterBit_Manufacturer:
+        reg.bits.manufacturerSpecific = 0;
+        break;
+    }
+    setValue(0, reg.value);
 }
 
 bool Object1001::isErrorfree()
--- src/objects/object_1001.hpp
+++ src/objects/object_1001.hpp
@@ -5,9 +5,6 @@ namespace CANopen
 {
     class Object1001 : public Object
     {
-    private:
-        void writeErrorBit(unsigned bit, bool value);
-
     public:
         Object1001(uint16_t index, uint8_t subNumber, uint16_t objectType, ObjectEntry *entries) : Object(index, subNumber, objectType, entries) {}
         uint8_t getValue();
