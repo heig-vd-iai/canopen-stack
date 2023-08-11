6f6dca0b3b12abc1c2e3a549c934ffc84c8218be
Tristan <tristan.lieberherr@heig-vd.ch>
2023-08-11 15:35:56 +0200

    check for TPDO mapping size

--- generator/objects/object_1A00.py
+++ generator/objects/object_1A00.py
@@ -5,19 +5,24 @@ class Object1A00(RecordObject):
     """Object 1A00: TPDO mapping parameter"""
     def __init__(self, index: int, entries: list[Variable]) -> None:
         super().__init__(index, entries, "Object1A00")
+        self._MAX_SIZE = 8
 
     def verify(self, objects: dict) -> bool:
-        ## Check that mapped objects exist
+        ## Check that mapped objects exist and size is valid
         retval = True
+        sizeSum = 0
         for i in range(self.entries[0].defaultValue):
             entry = self.entries[i + 1]
             index = entry.defaultValue >> 16 & 0xFFFF
             subindex = entry.defaultValue >> 8 & 0xFF
-            length = entry.defaultValue & 0xFF
+            sizeSum += (entry.defaultValue & 0xFF) // 8
             if index not in objects:
                 self.error(f"invalid TPDO mapping for sub {i + 1} (invalid index)")
                 retval = False
             elif objects[index].subNumber <= subindex:
                 self.error(f"invalid TPDO mapping for sub {i + 1} (invalid subindex)")
                 retval = False
+        if sizeSum > self._MAX_SIZE:
+            self.error(f"invalid size ({sizeSum})")
+            retval = False
         return retval
