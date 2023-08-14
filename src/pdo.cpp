2df377d55f27309ee3184483567d3c3d23f6cbfb
Tristan <tristan.lieberherr@heig-vd.ch>
2023-08-14 11:22:07 +0200

    implemented RPDO

--- src/pdo.cpp
+++ src/pdo.cpp
@@ -10,6 +10,8 @@ PDO::PDO(Node &node) : node(node)
 {
     for (unsigned i = 0; i < OD_TPDO_COUNT; i++)
         initTPDO(i);
+    for (unsigned i = 0; i < OD_RPDO_COUNT; i++)
+        initRPDO(i);
 }
 
 void PDO::initTPDO(unsigned index)
@@ -19,6 +21,13 @@ void PDO::initTPDO(unsigned index)
     remapTPDO(index);
 }
 
+void PDO::initRPDO(unsigned index)
+{
+    rpdos[index].commObject = (Object1400 *)node.od.findObject(RPDO_COMMUNICATION_INDEX + index);
+    rpdos[index].mapObject = (Object1600 *)node.od.findObject(RPDO_MAPPING_INDEX + index);
+    remapRPDO(index);
+}
+
 void PDO::remapTPDO(unsigned index)
 {
     TPDO *tpdo = tpdos + index;
@@ -26,7 +35,7 @@ void PDO::remapTPDO(unsigned index)
     tpdo->count = 0;
     if (tpdo->mappedEntries)
         delete[] tpdo->mappedEntries;
-    tpdo->mappedEntries = new TPDOPair[count];
+    tpdo->mappedEntries = new PDOPair[count];
     unsigned sizeSum = 0;
     for (unsigned i = 0; i < count; i++)
     {
@@ -34,13 +43,34 @@ void PDO::remapTPDO(unsigned index)
         Object *object = node.od.findObject(content.bits.index);
         sizeSum += object->getSize(content.bits.subindex);
         if (sizeSum > PDO_DLC)
-        {
             break;
-        }
         tpdo->mappedEntries[i].object = object;
         tpdo->mappedEntries[i].subindex = content.bits.subindex;
         tpdo->size = sizeSum;
         tpdo->count++;
+    }
+}
+
+void PDO::remapRPDO(unsigned index)
+{
+    RPDO *rpdo = rpdos + index;
+    unsigned count = rpdo->mapObject->getCount();
+    rpdo->count = 0;
+    if (rpdo->mappedEntries)
+        delete[] rpdo->mappedEntries;
+    rpdo->mappedEntries = new PDOPair[count];
+    unsigned sizeSum = 0;
+    for (unsigned i = 0; i < count; i++)
+    {
+        PDOMapEntry content = {rpdo->mapObject->getMappedValue(i)};
+        Object *object = node.od.findObject(content.bits.index);
+        sizeSum += object->getSize(content.bits.subindex);
+        if (sizeSum > PDO_DLC)
+            break;
+        rpdo->mappedEntries[i].object = object;
+        rpdo->mappedEntries[i].subindex = content.bits.subindex;
+        rpdo->size = sizeSum;
+        rpdo->count++;
     }
 }
 
@@ -56,6 +86,20 @@ void PDO::bufferizeTPDO(unsigned index, uint8_t *buffer)
         object->readBytes(subindex, buffer, size, bytesTransferred);
         bytesTransferred += size;
     }
+}
+
+void PDO::unpackRPDO(unsigned index, uint8_t *buffer)
+{
+    RPDO *rpdo = rpdos + index;
+    unsigned bytesTransferred = 0;
+    for (unsigned i = 0; i < rpdo->count; i++)
+    {
+        Object *object = rpdo->mappedEntries[i].object;
+        uint8_t subindex = rpdo->mappedEntries[i].subindex;
+        uint8_t size = object->getSize(subindex);
+        object->writeBytes(subindex, buffer + bytesTransferred, size, node);
+        bytesTransferred += size;
+    } // TODO: event-timer timeout
 }
 
 void PDO::sendTPDO(unsigned index, uint32_t timestamp_us)
@@ -84,23 +128,69 @@ void PDO::enable() { enabled = true; }
 
 void PDO::disable() { enabled = false; }
 
-void PDO::receiveFrame(Frame &frame, uint32_t timestamp_us)
+void PDO::receiveTPDO(Frame &frame, uint32_t timestamp_us)
 {
     if (!enabled || frame.nodeId != node.nodeId || !frame.rtr)
         return;
-    for (unsigned i = 0; i < OD_TPDO_COUNT; i++)
+    uint8_t index;
+    switch (frame.functionCode)
     {
-        TPDO *tpdo = tpdos + i;
-        uint8_t transmission = tpdo->commObject->getTransmissionType();
-        if (transmission == X1800_RTR_SYNC) // RTR sync
-        {
-            tpdo->syncFlag = true;
-        }
-        else if (transmission == X1800_RTR_EVENT) // RTR event-driven
-        {
-            sendTPDO(i, timestamp_us);
-        }
+    case FunctionCode_TPDO1:
+        index = 0;
+        break;
+    case FunctionCode_TPDO2:
+        index = 1;
+        break;
+    case FunctionCode_TPDO3:
+        index = 2;
+        break;
+    case FunctionCode_TPDO4:
+        index = 3;
+        break;
+    default:
+        return;
     }
+    TPDO *tpdo = tpdos + index;
+    uint8_t transmission = tpdo->commObject->getTransmissionType();
+    if (transmission == X1800_RTR_SYNC)
+        tpdo->syncFlag = true;
+    else if (transmission == X1800_RTR_EVENT)
+        sendTPDO(index, timestamp_us);
+}
+
+void PDO::receiveRPDO(Frame &frame, uint32_t timestamp_us)
+{
+    if (!enabled || frame.nodeId != node.nodeId)
+        return;
+    uint8_t index = 42;
+    switch (frame.functionCode)
+    {
+    case FunctionCode_RPDO1:
+        index = 0;
+        break;
+    case FunctionCode_RPDO2:
+        index = 1;
+        break;
+    case FunctionCode_RPDO3:
+        index = 2;
+        break;
+    case FunctionCode_RPDO4:
+        index = 3;
+        break;
+    default:
+        return;
+    }
+    RPDO *rpdo = rpdos + index;
+    if (rpdo->size != frame.dlc)
+        return;
+    uint8_t transmission = rpdo->commObject->getTransmissionType();
+    if (transmission <= X1400_SYNC_MAX)
+    {
+        memcpy(rpdo->buffer, frame.data, frame.dlc);
+        rpdo->syncFlag = true;
+    }
+    else
+        unpackRPDO(index, frame.data);
 }
 
 void PDO::update(uint32_t timestamp_us)
@@ -139,6 +229,14 @@ void PDO::onSync(uint8_t counter, uint32_t timestamp_us)
             sendTPDO(i, timestamp_us);
         }
     }
+    for (unsigned i = 0; i < OD_RPDO_COUNT; i++)
+    {
+        RPDO *rpdo = rpdos + i;
+        if (!rpdo->commObject->isSynchronous() || !rpdo->syncFlag)
+            continue;
+        unpackRPDO(i, rpdo->buffer);
+        rpdo->syncFlag = false;
+    }
 }
 
 void PDO::transmitTPDO(unsigned index)
@@ -147,11 +245,11 @@ void PDO::transmitTPDO(unsigned index)
         return;
     TPDO *tpdo = tpdos + index;
     uint8_t transmission = tpdo->commObject->getTransmissionType();
-    if (transmission == X1800_ACYCLIC) // synchronous acyclic
+    if (transmission == X1800_ACYCLIC)
     {
         tpdo->syncFlag = true;
     }
-    else if (transmission >= X1800_EVENT1) // event-driven
+    else if (transmission >= X1800_EVENT1)
     {
         uint32_t timestamp_us = node.getTime_us();
         bool supported = tpdo->commObject->isInhibitSupported();
@@ -162,8 +260,14 @@ void PDO::transmitTPDO(unsigned index)
     }
 }
 
-void PDO::reload()
+void PDO::reloadTPDO()
 {
     for (unsigned i = 0; i < OD_TPDO_COUNT; i++)
         remapTPDO(i);
+}
+
+void PDO::reloadRPDO()
+{
+    for (unsigned i = 0; i < OD_RPDO_COUNT; i++)
+        remapRPDO(i);
 }
--- src/pdo.hpp
+++ src/pdo.hpp
@@ -3,13 +3,15 @@
 #include <cstdint>
 #define TPDO_COMMUNICATION_INDEX 0x1800
 #define TPDO_MAPPING_INDEX 0x1A00
+#define RPDO_COMMUNICATION_INDEX 0x1400
+#define RPDO_MAPPING_INDEX 0x1600
 #define PDO_DLC 8
 
 namespace CANopen
 {
     class PDO
     {
-        struct TPDOPair
+        struct PDOPair
         {
             class Object *object;
             uint8_t subindex;
@@ -18,7 +20,18 @@ namespace CANopen
         {
             class Object1800 *commObject;
             class Object1A00 *mapObject;
-            TPDOPair *mappedEntries = nullptr;
+            PDOPair *mappedEntries = nullptr;
+            uint8_t count = 0;
+            uint8_t size = 0;
+            uint32_t timestamp_us = 0;
+            bool syncFlag = false;
+        };
+        struct RPDO
+        {
+            class Object1400 *commObject;
+            class Object1600 *mapObject;
+            PDOPair *mappedEntries = nullptr;
+            uint8_t buffer[PDO_DLC] = {0};
             uint8_t count = 0;
             uint8_t size = 0;
             uint32_t timestamp_us = 0;
@@ -29,10 +42,14 @@ namespace CANopen
         bool enabled = false;
         class Node &node;
         TPDO tpdos[OD_TPDO_COUNT];
+        RPDO rpdos[OD_RPDO_COUNT];
 
         void initTPDO(unsigned index);
+        void initRPDO(unsigned index);
         void remapTPDO(unsigned index);
+        void remapRPDO(unsigned index);
         void bufferizeTPDO(unsigned index, uint8_t *buffer);
+        void unpackRPDO(unsigned index, uint8_t *buffer);
         void sendTPDO(unsigned index, uint32_t timestamp_us);
         uint32_t getSyncWindow_us();
 
@@ -40,10 +57,12 @@ namespace CANopen
         PDO(class Node &node);
         void enable();
         void disable();
-        void receiveFrame(class Frame &frame, uint32_t timestamp_us);
+        void receiveTPDO(class Frame &frame, uint32_t timestamp_us);
+        void receiveRPDO(class Frame &frame, uint32_t timestamp_us);
         void update(uint32_t timestamp_us);
         void onSync(uint8_t counter, uint32_t timestamp_us);
         void transmitTPDO(unsigned index);
-        void reload();
+        void reloadTPDO();
+        void reloadRPDO();
     };
 }
