9bf3e0be1f963dba168630af793994273d16aca4
Tristan <tristan.lieberherr@heig-vd.ch>
2023-08-14 07:28:30 +0200

    added objects 1400 and 1600

---
+++ generator/objects/object_1400.py
@@ -0,0 +1,13 @@
+from canopen.objectdictionary import Variable
+from .generic import RecordObject
+
+class Object1400(RecordObject):
+    """Object 1400: RPDO communication parameter"""
+    def __init__(self, index: int, entries: list[Variable]) -> None:
+        super().__init__(index, entries, "Object1400")
+        ## Check for valid sub number
+        errors = False
+        if self.subNumber < 3:
+            self.error(f"at least 3 entries should be present")
+            errors = True
+        if errors: raise ValueError()
---
+++ generator/objects/object_1600.py
@@ -0,0 +1,28 @@
+from canopen.objectdictionary import Variable
+from .generic import RecordObject
+
+class Object1600(RecordObject):
+    """Object 1600: RPDO mapping parameter"""
+    def __init__(self, index: int, entries: list[Variable]) -> None:
+        super().__init__(index, entries, "Object1600")
+        self._MAX_SIZE = 8
+
+    def verify(self, objects: dict) -> bool:
+        ## Check that mapped objects exist and size is valid
+        retval = True
+        sizeSum = 0
+        for i in range(self.entries[0].defaultValue):
+            entry = self.entries[i + 1]
+            index = entry.defaultValue >> 16 & 0xFFFF
+            subindex = entry.defaultValue >> 8 & 0xFF
+            sizeSum += entry.size
+            if index not in objects:
+                self.error(f"invalid RPDO mapping for sub {i + 1} (invalid index)")
+                retval = False
+            elif objects[index].subNumber <= subindex:
+                self.error(f"invalid RPDO mapping for sub {i + 1} (invalid subindex)")
+                retval = False
+        if sizeSum > self._MAX_SIZE:
+            self.error(f"invalid size ({sizeSum})")
+            retval = False
+        return retval
