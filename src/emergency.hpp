b30a699a8b1217a12e63e5b4b895735c9f3e5574
Tristan <tristan.lieberherr@heig-vd.ch>
2023-08-29 08:12:58 +0200

    added Doxygen comments

--- src/emergency.hpp
+++ src/emergency.hpp
@@ -19,17 +19,58 @@ namespace CANopen
         class Node &node;
         class Object1001 *errorRegisterObject;
 
+        /**
+         * @brief Enable EMCY functionality, should be used by NMT.
+         */
         void enable();
+
+        /**
+         * @brief Disable EMCY functionality, should be used by NMT.
+         */
         void disable();
+
+        /**
+         * @brief Internal method to send an error message.
+         * @param errorCode The error code to send.
+         * @param manufacturerCode The manufacturer-specific error code.
+         */
         void sendError(uint16_t errorCode, uint32_t manufacturerCode);
 
     public:
         friend class NMT;
+        /**
+         * @brief Constructor for EMCY class. This class handles the emission of emergency messages, as well as the pre-defined error field and error register.
+         * @param node The parent Node instance.
+         */
         EMCY(class Node &node);
+
+        /**
+         * @brief Sends an emergency message on the bus, and automatically sets the corresponding bits in the error register. The generic error bit is always set when using this method.
+         * @param errorCode The error code to send.
+         * @param manufacturerCode The optional manufacturer-specific error code.
+         */
         void raiseError(uint16_t errorCode, uint16_t manufacturerCode = 0);
+
+        /**
+         * @brief Clear a specific error bit from the error register. The generic error bit can only be cleared if all other bits are clear.
+         * @param bit The index of the error bit to clear.
+         */
         void clearErrorBit(unsigned bit);
+
+        /**
+         * @brief Get the current error register value.
+         * @return The error register value.
+         */
         uint8_t getErrorRegister();
+
+        /**
+         * @brief Clear the pre-defined error field (error history) in object 0x1003.
+         */
         void clearHistory();
+
+        /**
+         * @brief Completely reset the error register to 0.
+         */
         void reset();
     };
 }
