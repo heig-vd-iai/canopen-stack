69e386f0fe4a3821f60ece15cb082650f481b3d8
Tristan <tristan.lieberherr@heig-vd.ch>
2023-12-13 06:29:00 +0100

    Revert "separated classes with modified #ifndef"
    This reverts commit 3c0035ac59e7154f20de42d8818b7ccfd235aa8d.

--- example/od.hpp
+++ example/od.hpp
@@ -303,7 +303,10 @@
 #define OD_MUX_60FE_SUB_0 0x60FE00
 #define OD_MUX_60FE_SUB_1 0x60FE01
 
-#include <cstdint>
+/* Useful if only the defines from this file are needed */ 
+#ifndef _OD_DEFINES_ONLY
+#include "od_include.hpp"
+
 namespace CANopen
 {
 /**
@@ -385,13 +388,7 @@ struct ObjectDictionnaryData
     uint8_t x60FEsub0 = 1;
     uint32_t x60FE[1] = {0};
 };
-}
 
-/* Useful if the ObjectDictionnary definition is not needed */ 
-#ifndef _OD_NO_DICTIONNARY
-#include "od_include.hpp"
-namespace CANopen
-{
 /**
  * Object Dictionary.
  * It is an auto-generated class that should not be manually edited.
@@ -813,4 +810,4 @@ public:
     bool restoreData(uint8_t parameterGroup);
 };
 }
-#endif // _OD_NO_DICTIONNARY
+#endif // _OD_DEFINES_ONLY
