583dd19b9c4e621e4ac89d17f34e367c49b1c9c9
Tristan Lieberherr <tristan.lieberherr@heig-vd.ch>
2024-01-11 14:43:01 +0100

    added todo in __main__.py

--- generator/__main__.py
+++ generator/__main__.py
@@ -5,12 +5,11 @@ import yaml
 
 @click.command()
 @click.argument('filename', type=click.Path(exists=True))
-# @click.argument('--cpp_filename', default='od')
-# @click.argument('--eds_filename', default='datasheet')
 def cli(filename=None):
     """ This program converts a valid YAML file into an C++ header file and an EDS data sheet."""
     with open(filename) as f:
         od = ObjectDictionary(yaml.safe_load(f))
+    # TODO: arguments for output file names
     with open('od.hpp', 'w') as f:
         f.write(od.to_cpp())
     with open('datasheet.eds', 'w') as f:
